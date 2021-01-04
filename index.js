922eebb4f8fd9082d078c16d1c435f259ed1a8dc
Abhishek Samariya <asamariya@gmail.com>
2021-01-04 15:04:08 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+// set up express
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`The server has started on PORT: ${PORT}`));
